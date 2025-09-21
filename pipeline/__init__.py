9b913a44925d6d986345360777bc4e5dc1d857d8
yvantchachuang-star <yvan.tchachuang@gmail.com>
2025-09-21 18:34:22 +0200

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,6 +1,7 @@
 from .analyse_roles import analyse_roles
 from .gen_objectifs import gen_objectifs
 from .gen_story import gen_story
+from .gen_exigences import gen_exigences
 
 def generer_stories_depuis_besoin(requete: str) -> list:
     rôles = analyse_roles(requete)
@@ -8,6 +9,8 @@ def generer_stories_depuis_besoin(requete: str) -> list:
     for rôle in rôles:
         objectifs = gen_objectifs(rôle)
         for obj in objectifs:
-            stories.append(gen_story(rôle, obj))
+            bloc = gen_story(rôle, obj)
+            bloc["exigences"] = gen_exigences(rôle, obj)
+            bloc["babok"] = f"Exigence métier identifiée pour le rôle {rôle}, liée à l'objectif : {obj}."
+            stories.append(bloc)
     return stories
-
