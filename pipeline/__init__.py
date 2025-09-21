0f7986e8521f173f6fdde8d107c4def603364ba4
yvantchachuang-star <yvan.tchachuang@gmail.com>
2025-09-21 18:15:50 +0200

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,1 +1,13 @@
+from .analyse_roles import analyse_roles
+from .gen_objectifs import gen_objectifs
+from .gen_story import gen_story
+
+def generer_stories_depuis_besoin(requete: str) -> list:
+    rôles = analyse_roles(requete)
+    stories = []
+    for rôle in rôles:
+        objectifs = gen_objectifs(rôle)
+        for obj in objectifs:
+            stories.append(gen_story(rôle, obj))
+    return stories
 
