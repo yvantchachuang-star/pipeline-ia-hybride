bf5ee8f5c6fca00d10fb2a88c1fe25af40f4a06f
yvantchachuang-star <yvan.tchachuang@gmail.com>
2025-09-21 18:13:25 +0200

    Update analyse_roles.py

--- pipeline/analyse_roles.py
+++ pipeline/analyse_roles.py
@@ -1,1 +1,7 @@
+import re
 
+def analyse_roles(requete: str) -> list:
+    candidats = re.findall(r"\ble (\w+)", requete.lower()) + \
+                re.findall(r"\bla (\w+)", requete.lower()) + \
+                re.findall(r"\ben tant que (\w+)", requete.lower())
+    return list(set(candidats)) or ["utilisateur"]
