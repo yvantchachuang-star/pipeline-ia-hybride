b0ada65df0cd8144626fcd4f0f6c6e0098ca60e8
yvantchachuang-star <yvan.tchachuang@gmail.com>
2025-09-23 17:27:52 +0200

    Update interaction_engine.py

--- pipeline/interaction_engine.py
+++ pipeline/interaction_engine.py
@@ -2,6 +2,5 @@ from pipeline.intention_engine import detecter_intention
 from pipeline.assistant_engine import repondre_intelligemment
 
 def repondre_chat(message: str, stories: list) -> str:
-    # Optionnel : détecter l’intention pour enrichir plus tard
     intention = detecter_intention(message)
     return repondre_intelligemment(message, stories)
